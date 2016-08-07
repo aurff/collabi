b26027130080cff693a1bc8aa4f280bbea02fa2e
Andih <lavalampeh@gmail.com>
2016-08-07 14:33:14 +0200

    created a controller for the main site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'site/index'
+  root to: 'site#index'
 end
