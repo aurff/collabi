b2b106dbd28a9b6ad516ca2b4b2808f13ae98756
sec8 <lavalampeh@gmail.com>
2016-08-20 13:14:16 +0200

    Set up chatroom related routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chatrooms do
+    resources :chatroom_users
+  end
+
   get 'site/index'
   root to: 'site#index'
 end
