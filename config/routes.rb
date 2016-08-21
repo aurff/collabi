b32210f3742e0e42f8b614da33f6092c42591f06
sec8 <lavalampeh@gmail.com>
2016-08-21 14:59:38 +0200

    Add chatroom-users-controller with join/leave function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :chatrooms do
-    resources :chatroom_users
+    resource :chatroom_users
   end
 
   get 'site/index'
