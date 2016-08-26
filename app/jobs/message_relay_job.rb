9502ad1dfb20e4aa96f4787cfe83e84f3f6738c4
sec8 <lavalampeh@gmail.com>
2016-08-26 16:33:16 +0200

    Fix some typos & rendering issues

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -2,9 +2,9 @@ class MessageRelayJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "chatroom:#{message.chatroom.id}",{
-      message: MessageController.render(message),
+    ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}",{
+      message: MessagesController.render(message),
       chatroom_id: message.chatroom.id
-    } 
+    }
   end
 end
