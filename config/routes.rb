e0a29104085376112d94e4a6bf7fd36833e83ed6
Arne Urff <arne.urff@gmx.de>
2016-09-19 15:07:16 +0200

    added root changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'site/index'
+  #get 'site/index'
+  
+  
+  controller :site do
+    get 'site/index'    => :index
+    get 'site/finder'   => :finder
+  end
+  
+  controller :groups do
+    get 'groups/index'  => :index
+    get 'groups/new'    => :new
+    get 'groups/show'   => :show
+    get 'groups/showAll'  =>:showAll
+  end
+  
+  resources :groups do
+    post :addUserToGroup, :on       => :collection
+    post :removeUserFromGroup, :on  => :collection
+  end
+  
   root to: 'site#index'
 end
